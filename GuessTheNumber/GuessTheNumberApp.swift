9a7ed197b13222e35d949bc74c4d2711fb62b821
Marat Khayrullin <m.f.khayrullin@gmail.com>
2022-05-23 19:46:00 +0300

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by MAC  on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
